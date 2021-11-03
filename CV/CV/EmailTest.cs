199c8a462d9218ff685845b175cd6dc46d347b25
Berk CATAL <berk99@live.dk>
2021-11-03 08:13:28 +0100

    Add project files.

---
+++ CV/CV/EmailTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CV
+{
+    public class EmailTest
+    {
+        private void WebUrl_TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var label = sender as Label;
+
+            string url = "http://" + label.Text;
+
+            var address = "merk68b@gmail.com";
+            Device.OpenUri(new Uri($"mailto:{address}"));
+        }
+    }
+}
